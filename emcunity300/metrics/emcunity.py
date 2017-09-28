01b2e86cee7d36fad9655ee5d4465d7ffb8969e7
Marco Lenhardt <marco@lenhardt.in>
2017-09-28 15:16:23 +0200

    added fs part

--- emcunity300/metrics/emcunity.py
+++ emcunity300/metrics/emcunity.py
@@ -3,4 +3,5 @@
 
 check_metrics["check_mk-emcunity_pool"]			= df_translation
 check_metrics["check_mk-emcunity_lun"]				= df_translation
+check_metrics["check_mk-emcunity_fs"]				= df_translation
 check_metrics["check_mk-emcunity_vmfs"]				= df_translation
