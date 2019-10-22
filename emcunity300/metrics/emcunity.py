6d3c6e8d502e7da7efb8384ee4173edb060a58d0
Philipp N <31624428+ImTheLucKyOne@users.noreply.github.com>
2019-10-22 18:03:09 +0200

    Added import of df_translation for cmk1.6

--- emcunity300/metrics/emcunity.py
+++ emcunity300/metrics/emcunity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
+from cmk.gui.plugins.metrics.check_mk import df_translation
+
 check_metrics["check_mk-emcunity_pool"]			= df_translation
 check_metrics["check_mk-emcunity_lun"]				= df_translation
 check_metrics["check_mk-emcunity_fs"]				= df_translation
