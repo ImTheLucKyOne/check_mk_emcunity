4ff659274766336367a0521f01c9fd56f7f9685e
Andreas Doehler <andreas.doehler@gmail.com>
2021-05-03 22:00:34 +0200

    First migration step for 2.0

--- emcunity300/metrics/emcunity.py
+++ emcunity300/metrics/emcunity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-from cmk.gui.plugins.metrics.check_mk import df_translation
+from cmk.gui.plugins.metrics.translation import df_translation
 
-check_metrics["check_mk-emcunity_pool"]			= df_translation
-check_metrics["check_mk-emcunity_lun"]				= df_translation
-check_metrics["check_mk-emcunity_fs"]				= df_translation
-check_metrics["check_mk-emcunity_vmfs"]				= df_translation
+check_metrics["check_mk-emcunity_pool"] = df_translation
+check_metrics["check_mk-emcunity_lun"] = df_translation
+check_metrics["check_mk-emcunity_fs"] = df_translation
+check_metrics["check_mk-emcunity_vmfs"] = df_translation
